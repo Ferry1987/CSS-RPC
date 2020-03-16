51209a9c97cc5068aa0c02b1910bc3402cfe0397
Ferry1987 <33051616+Ferry1987@users.noreply.github.com>
2020-03-16 15:31:56 +0800

    clean up project

--- producer/src/main/java/com/css/com/css/protocol/AbstractRemoteInvoker.java
+++
@@ -1,7 +0,0 @@
-package com.css.com.css.protocol;
-
-import com.css.com.css.rpc.ProducerStandardServer;
-
-public abstract class AbstractRemoteInvoker implements ProducerStandardServer {
-
-}
--- producer/src/main/java/com/css/com/css/rpc/RemoteInvoker.java
+++
@@ -1,8 +0,0 @@
-package com.css.com.css.rpc;
-
-public interface  RemoteInvoker {
-    /***
-     *接收请求，调用服务
-     */
-    public void invoke();
-}
